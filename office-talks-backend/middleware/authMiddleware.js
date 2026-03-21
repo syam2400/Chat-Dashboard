efb16655bdd378fe099b8334915c3d6bfb63f9da
syam2400 <syam2400@gmail.com>
2026-03-21 20:49:21 +0530

    added some api fixes

--- office-talks-backend/middleware/authMiddleware.js
+++ office-talks-backend/middleware/authMiddleware.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
+   const authHeader = req.header('Authorization');
+
   // Get token from header
-  const token = req.header('x-auth-token');
-  if (!token) {
+if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
+
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
